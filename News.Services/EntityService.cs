87e5dd5d9346f1b3bd50a34ce6a1762f361440ed
batuhan güneç <batuhangunec@outlook.com>
2020-01-15 21:57:48 +0300

    News Website(under construction)

---
+++ News.Services/EntityService.cs
@@ -0,0 +1,28 @@
+﻿using News.BLL.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace News.Services
+{
+    public class EntityService
+    {
+        public EntityService()
+        {
+            _allNewsRepository = new AllNewsRepository();
+            _categoryService = new CategoryRepository();
+            _appUserService = new AppUserRepository();
+
+        }
+        private CategoryRepository _categoryService;
+        public CategoryRepository CategoryService { get { return _categoryService; } set { _categoryService = value; } }
+
+        private AppUserRepository _appUserService;
+        public AppUserRepository AppUserService { get { return _appUserService; } set { _appUserService = value; } }
+
+        private AllNewsRepository _allNewsRepository;
+        public AllNewsRepository AllNewsService { get { return _allNewsRepository; } set { _allNewsRepository = value; } }
+    }
+}
